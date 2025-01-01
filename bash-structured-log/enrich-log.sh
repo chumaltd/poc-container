86fb95daa55d47298ebe30f6c0722ccbc9513336
Chuma Takahiro <chuma@unicorn.limited>
2025-01-01 18:24:13 +0900

    replace sed -z w/ tr for lacking gsed env like busybox

--- bash-structured-log/enrich-log.sh
+++ bash-structured-log/enrich-log.sh
@@ -6,10 +6,10 @@ SPAN_ID="000000000000$(printf '%04x' $RANDOM)"
 
 #NOTE: https://en.wikipedia.org/wiki/Process_substitution
 #NOTE: rewrite STDOUT
-exec 1> >(echo -E "{ \"traceId\": \"$TRACE_ID\", \"spanId\": \"$SPAN_ID\", \"message\": \"[$SERVICE_NAME] $(sed -z -e 's/"/\\"/g' -e 's/\n/\\n/g')\", \"attributes\": { \"service.name\": \"$SERVICE_NAME\" } }")
+exec 1> >(echo -E "{ \"traceId\": \"$TRACE_ID\", \"spanId\": \"$SPAN_ID\", \"message\": \"[$SERVICE_NAME] $(tr '\n' '\\n' | sed -e 's/"/\\"/g')\", \"attributes\": { \"service.name\": \"$SERVICE_NAME\" } }")
 
 #NOTE: rewrite STDERR
-exec 2> >(echo -E "{ \"traceId\": \"$TRACE_ID\", \"spanId\": \"$SPAN_ID\", \"message\": \"[$SERVICE_NAME] $(sed -z -e 's/"/\\"/g' -e 's/\n/\\n/g')\", \"attributes\": { \"service.name\": \"$SERVICE_NAME\" } }" >&2)
+exec 2> >(echo -E "{ \"traceId\": \"$TRACE_ID\", \"spanId\": \"$SPAN_ID\", \"message\": \"[$SERVICE_NAME] $(tr '\n' '\\n' | sed -e 's/"/\\"/g')\", \"attributes\": { \"service.name\": \"$SERVICE_NAME\" } }" >&2)
 
 
 echo "log to STDOUT"
